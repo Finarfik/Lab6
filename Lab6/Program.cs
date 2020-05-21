36b0c15c92054de26920b93d8f34e3e0deae37b7
Finarfik <makakid08@mail.ru>
2020-05-21 21:38:39 +0300

    Добавьте файлы проекта.

---
+++ Lab6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
+            double F = 2 * Math.Sin(x) + 3 * Math.Cos(x);
+            Console.WriteLine(F);
+            Console.ReadKey();
+
+        }
+    }
+}
