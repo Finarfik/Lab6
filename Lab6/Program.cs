6267bee35f16964230bf354785998a118e0e0f78
Finarfik <makakid08@mail.ru>
2020-05-21 21:43:10 +0300

    ver 0.2

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -10,10 +10,11 @@ namespace Lab6
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите значение параметра х: ");
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
             double F = 2 * Math.Sin(x) + 3 * Math.Cos(x);
-            Console.WriteLine(F);
+            Console.WriteLine($"Значение F = {F}.");
             Console.ReadKey();
 
         }
